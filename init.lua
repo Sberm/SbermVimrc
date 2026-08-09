961ef9084a2e27f0ed71dcc9a066c8c8d60b0578
Howard Chu <howardchu95@gmail.com>
2026-08-09 18:01:23 +0000

    Use OSC 52
    Signed-off-by: Howard Chu <howardchu95@gmail.com>

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- clipboard: OSC 52
+vim.g.clipboard = 'osc52'
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
